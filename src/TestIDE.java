c6641d3cfea4ee359cd34181061872872a0765f9
cada16 <te16.daniel.carlsson@umea.nti.se>
2017-11-06 09:48:02 +0100

    Hej

--- src/TestIDE.java
+++ src/TestIDE.java
@@ -8,5 +8,6 @@ import javax.swing.*;
 public class TestIDE {
     public static void main(String[] args) {
         System.out.println("Hello World");
+        JOptionPane.showMessageDialog(null,"Hej");
     }
 }

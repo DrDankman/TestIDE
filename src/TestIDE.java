2331adc638d84caea2ab2224582f013beb32a5ea
DrDankman <imdanielc@gmail.com>
2017-11-06 23:23:01 +0100

    Spelet klart Gissa tal mellan 0 och 100

--- src/TestIDE.java
+++ src/TestIDE.java
@@ -1,13 +1,27 @@
 import javax.swing.*;
 
 /*
-    Hello World
+    Gissa tal mellan 0 och 100 spel
     Daniel Carlsson
     2017-11-06
 */
 public class TestIDE {
     public static void main(String[] args) {
-        System.out.println("Hello World");
-        JOptionPane.showMessageDialog(null,"Hej");
+
+        double random = Math.random();
+        int number = (int) (random * 100) + 1;
+        int ggr = 1;
+        while(ggr<11){
+        int forsta = Integer.parseInt(JOptionPane.showInputDialog(null, "Jag tänker på ett tal mellan 0 och 100, gissa vilket.\nFörsök " + ggr));
+        if (forsta == number) {
+            JOptionPane.showMessageDialog(null, "Grattis! Du fick rätt! \nDet tog " + ggr + " försök");
+            System.exit(0);
+        } else if (forsta > number) {
+            JOptionPane.showMessageDialog(null, "Talet är mindre, gissa igen");
+        } else if (forsta < number) {
+            JOptionPane.showMessageDialog(null, "Talet är större, gissa igen");
+        }
+        ggr++;
+    }
     }
 }

ca0676a919722bcf6fb16f81cd492ba381f60d49
Daniel Carlsson <cada16@nti.umea>
2017-11-06 09:44:12 +0100

    Initial commit

---
+++ src/TestIDE.java
@@ -0,0 +1,12 @@
+import javax.swing.*;
+
+/*
+    Hello World
+    Daniel Carlsson
+    2017-11-06
+*/
+public class TestIDE {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
